1cbb1ac83a371bb4ec9cad0687beb4d4c4b48db7
mohammad <ababdoiuy@gmail.com>
2019-06-16 04:39:42 +0300

    Realationchip created

--- app/Review.php
+++ app/Review.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App;
-
+use App\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+        public function product(){
+            return $this->belongsTo(Product::class);
+        }
 }
