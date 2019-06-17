a0ba035dcd6822931f8d39a38232f8e6e881b5a2
mohammad <ababdoiuy@gmail.com>
2019-06-17 04:39:52 +0300

    Modified Product details

--- app/Http/Resources/Product/ProductResouce.php
+++ app/Http/Resources/Product/ProductResouce.php
@@ -18,8 +18,13 @@ class ProductResouce extends JsonResource
             'name'=> $this->name,
             'description'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
-            'discount'=>$this->discount
+            'stock'=>$this->stock == 0 ?'out of stock':$this->stock,
+            'discount'=>$this->discount,
+            'totalPrice'=>$this->price * (1-($this->discount/100)),
+            'rating'=>$this->reviews->count()>0?round($this->reviews->sum('star')/$this->reviews->count(),2):'No rating yet',
+            'href'=>[
+                'reviews'=>route('reviews.index',$this->id)
+            ]
         ];
     }
 }
